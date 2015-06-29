da18221e2317deaf8f23f29841ca07e7a481ee04
erwin06 <erwin.bader06@gmail.com>
2015-06-29 20:43:10 -0300

    ultimamte

--- blackhole/bd.php
+++ blackhole/bd.php
@@ -9,6 +9,7 @@
 	$dbpass = 'meDUtozu49';
 	$dbname = 'm2000315_Radio';
 
+
     // $dbhost = 'localhost';
     // $dbuser = 'root';
     // $dbpass = '';
@@ -21,6 +22,7 @@
 	// $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Ocurrio un error al conectarse al servidor mysql');
 	// mysql_select_db($dbname);
 
+    $quer = "DROP TABLE IF EXISTS 'configuration'; CREATE TABLE 'configuration' ('idconfiguration' int(11) NOT NULL AUTO_INCREMENT,'frontphoto' varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,PRIMARY KEY ('idconfiguration')) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;";
 
     $result = $mysqli->query($quer);
 
@@ -29,6 +31,42 @@
     } else {
        echo $mysqli->error;
     }
+
+    $quer = "LOCK TABLES `configuration` WRITE; INSERT INTO `configuration` VALUES (1,'portada2 (9).jpg'); UNLOCK TABLES;";
+
+    $result = $mysqli->query($quer);
+
+    if($result === TRUE){
+        echo 'Se ejecutó con éxito1';
+    } else {
+       echo $mysqli->error;
+    }
+
+    $quer = "LOCK TABLES `configuration` WRITE; INSERT INTO `configuration` VALUES (1,'portada2 (9).jpg'); UNLOCK TABLES;";
+
+    $result = $mysqli->query($quer);
+
+    if($result === TRUE){
+        echo 'Se ejecutó con éxito2';
+    } else {
+       echo $mysqli->error;
+    }
+
+
+
+    $quer = "DROP TABLE IF EXISTS `user`;CREATE TABLE `user` (`iduser` int(11) NOT NULL AUTO_INCREMENT,`user` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,`admin` int(11) NOT NULL,`password` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,`idsession` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,PRIMARY KEY (`iduser`)) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci; LOCK TABLES `user` WRITE; INSERT INTO `user` VALUES (1,'admin',1,'202cb962ac59075b964b07152d234b70','9d07cbc297ffeb7ca7e3958e99da1b79');UNLOCK TABLES;";
+
+
+    $result = $mysqli->query($quer);
+
+    if($result === TRUE){
+        echo 'Se ejecutó con éxito3';
+    } else {
+       echo $mysqli->error;
+    }
+
+
+    
 
     // var_dump($result);
     // // Prepara la consulta
